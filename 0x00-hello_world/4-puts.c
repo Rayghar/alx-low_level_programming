b6ea9c077b045a048a47dfda80bf78e321f397fe
Rayghar <chukwuneme@gmail.com   qexitcd ..git statusclosecat README.md ls:q!?>
2023-02-17 15:04:02 -0800

    y

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #!/bin/bash
-#include <stdio.h>
-
+echo '#include <stdio.h>
 int main() {
     puts("Programming is like building a multilingual puzzle");
     return 0;
-}
+}' > program.c
+gcc program.c -o program && ./program && rm program program.c
