a4a54ddf34268793375153183c7a93bc70de025d
Rayghar <chukwuneme@gmail.com   qexitcd ..git statusclosecat README.md ls:q!?>
2023-02-21 04:14:17 -0800

    y

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,18 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**/
+/**
+ * Initializes random number generator
+ * to a random number between 
+ * and prints whether the number is positive, zero, or negative
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("The number %d is ", n);
+	printf("%d is ", n);
 	if (n > 0)
         	printf("positive.\n");
 	else if (n == 0)
