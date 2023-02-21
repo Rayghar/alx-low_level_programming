73e612f653c0679d4d33bf9899d1f6139ebcad36
Rayghar <chukwuneme@gmail.com   qexitcd ..git statusclosecat README.md ls:q!?>
2023-02-21 06:09:21 -0800

    y

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,11 +15,11 @@ int main(void)
 	last_digit = n % 10;
 	printf("Last digit of %d is ", n);
 	if (last_digit > 5)
-		printf("%d and is greater than 5\n", last_digit);
+		printf("%d and is greater than 5", last_digit);
 	else if (last_digit == 0)
-		printf("%d and is 0\n", last_digit);
+		printf("%d and is 0", last_digit);
 	else
-		printf("%d less than 6 and not 0\n", last_digit);
+		printf("%d less than 6 and not 0", last_digit);
 	printf("\n");
 	return (0);
 }
