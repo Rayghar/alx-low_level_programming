2180817d73fa0150a681bf7379f78d9e7f1417f3
Rayghar <chukwuneme@gmail.com   qexitcd ..git statusclosecat README.md ls:q!?>
2023-02-21 10:50:36 -0800

    y

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,18 +8,22 @@
  */
 int main(void)
 {
-	int n, last_digit;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
-	printf("Last digit of %d is ", n);
-	if (last_digit > 5)
-		printf("%d and is greater than 5", last_digit);
-	else if (last_digit == 0)
-		printf("%d and is 0", last_digit);
-	else
-		printf("%d and is less than 6 and not 0", last_digit);
-	printf("\n");
+	char letter  = 'a';
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+		{	putchar(leter);
+		}
+		letter++;
+	}
+	letter  = 'A';
+	while (letter <= 'Z')
+	{
+		if (letter != 'Q' && letter != 'E')
+		{	putchar(letter);
+		}
+		letter++;
+	}
+	putchar('\n');
 	return (0);
 }
