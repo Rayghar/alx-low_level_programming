014b1526e80437bf3d39109c1eb47d59883ac2f6
Rayghar <chukwuneme@gmail.com   qexitcd ..git statusclosecat README.md ls:q!?>
2023-02-21 04:43:33 -0800

    y

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,20 +6,18 @@
  *
  * Return: 0 on success, non-zero value on failure
  */
-
 int main(void)
 {
-        int n;
-
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        printf("%d is", n);
-        if (n > 0)
-                printf("positive\n");
-        else if (n == 0)
-                printf("zero\n");
-        else
-                printf("negative\n");
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d ", n);
+	if (n > 0)
+ 		printf("is positive\n");
+	else if (n == 0)
+		printf("is zero\n");
+	else
+		printf("is negative\n");
         printf("\n");
-        return (0);
+	return (0);
 }
