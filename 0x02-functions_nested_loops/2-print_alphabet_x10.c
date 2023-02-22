75166a8018fe248a9b4ade43f9c751375975174e
Rayghar <chukwuneme@gmail.com   qexitcd ..git statusclosecat README.md ls:q!?>
2023-02-21 16:39:57 -0800

    y

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,20 @@
-#include "main.h"
-
+#include <main.h>
 /**
- * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
+ * print_alphabet_x10 - Prints the alphabet 10 times
+ *
+ * Return: void
  */
 void print_alphabet_x10(void)
 {
-	int i, j;
+    char c;
+    int i;
 
-	for (i = 0; i < 10; i++) /* Loop for 10 times */
-	{
-		for (j = 'a'; j <= 'z'; j++) /* Loop for each letter */
-		{
-			_putchar(j); /* Print the letter */
-                }
-		_putchar('\n'); /* Print a new line after each set of letters */
-	}
+    for (i = 0; i < 10; i++)
+    {
+        for (c = 'a'; c <= 'z'; c++)
+        {
+            _putchar(c);
+        }
+        _putchar('\n');
+    }
 }
