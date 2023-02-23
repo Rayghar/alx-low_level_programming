83399ef069c0685c118250d29c0548cfc08bf14b
Rayghar <chukwuneme@gmail.com   qexitcd ..git statusclosecat README.md ls:q!?>
2023-02-23 15:28:58 -0800

    y

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _putchar(char c);
+int _islower(int c);
+int main(void);
+int _isalpha(int c);
+int print_sign(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
