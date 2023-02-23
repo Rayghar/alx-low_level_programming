61da1c967ac7f5a80e16109d91456bac8e1cc45d
Rayghar <chukwuneme@gmail.com   qexitcd ..git statusclosecat README.md ls:q!?>
2023-02-22 16:31:08 -0800

    y

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,27 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 #include <unistd.h>
 
 /**
- * print_alphabet - Prints the alphabet in lowercase followed by a new line
+ * print_alphabet - Prints the alphabet in lowercase, followed by a new line.
  */
 void print_alphabet(void)
 {
-        char letter;
+	char letter;
 
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-                _putchar(letter);
-        }
-        _putchar('\n');
-}
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
 }
