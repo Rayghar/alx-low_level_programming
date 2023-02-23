a3d36932b8cb5552a754bce8ad75fce7132a7ad6
Rayghar <chukwuneme@gmail.com   qexitcd ..git statusclosecat README.md ls:q!?>
2023-02-22 18:09:06 -0800

    y

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * _islower - Checks for lowercase character
- * @c: The character to be checked
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
  *
- * Return: 1 if c is lowercase, 0 otherwise
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
 int _isalpha(int c)
 {
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,4 @@ int _putchar(char c);
 int _islower(int c);
 int main(void);
 int _isalpha(int c);
+int print_sign(int n);
