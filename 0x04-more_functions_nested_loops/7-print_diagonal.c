536e514eb017c18a8baffacdaf23094d48a00468
Rayghar <chukwuneme@gmail.com   qexitcd ..git statusclosecat README.md ls:q!?>
2023-02-23 16:52:25 -0800

    y

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,13 +2,30 @@
 #include <stdio.h>
 
 /**
- * print_numbers - prints the numbers 0 to 9, followed by a new line
+ * print_diagonal - prints a diagonal line of backslashes in the terminal
+ * @n: number of times to print the diagonal character ('\')
+ *
+ * Description: This function prints a diagonal line of backslashes in the terminal
+ * with leading spaces for each row. The diagonal line ends with a newline character.
+ * If n is 0 or less, the function only prints a newline character.
+ *
+ * Return: void
  */
-void print_numbers(void)
+void print_diagonal(int n)
 {
-	int i;
+	if (n <= 0)
+       	{
+		_putchar('\n');  // if n is 0 or less, only print a newline
+		return;
+    	}
 
-	for (i = 0; i <= 9; i++)
-		_putchar(i + '0');
-	_putchar('\n');
+	for (int i = 0; i < n; i++) 
+	{
+		for (int j = 0; j < i; j++) 
+		{
+			_putchar(' ');  // print spaces for all columns before the diagonal
+        	}
+        _putchar('\\');  // print the diagonal character for the i-th row
+        _putchar('\n');  // end the row with a newline
+    	}
 }
