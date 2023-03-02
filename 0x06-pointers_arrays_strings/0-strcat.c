9477e97eafa10a9d8fbf7bd6ebd064a50c03588b
Rayghar <chukwuneme@gmail.com   qexitcd ..git statusclosecat README.md ls:q!?>
2023-03-02 09:27:11 -0800

    y

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,16 +14,12 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	/* Find the end of the destination string */
 	for (i = 0; dest[i] != '\0'; i++);
 
-        /* Append the source string to the destination string */
 	for (j = 0; src[j] != '\0'; j++)
 		dest[i + j] = src[j];
 
-	/* Add the terminating null byte */
 	dest[i + j] = '\0';
 
-	/* Return a pointer to the resulting string */
 	return dest;
 }
