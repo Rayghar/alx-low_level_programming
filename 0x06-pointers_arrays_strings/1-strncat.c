e385801b8d600122043bd90feb4433e0201bec5c
Rayghar <chukwuneme@gmail.com   qexitcd ..git statusclosecat README.md ls:q!?>
2023-03-02 09:59:11 -0800

    y

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,33 +1,28 @@
 #include "main.h"
 #include <stdio.h>
-#include <stddef.h>
 
 /**
- * _strncat - concatenates two strings, up to n bytes from src
- * @dest: pointer to destination string
- * @src: pointer to source string
- * @n: maximum number of bytes to concatenate from src
+ * _strncat - concatenates two strings
+ * @dest: the destination string
+ * @src: the source string
+ * @n: the maximum number of bytes to concatenate from src
  *
- * Return: pointer to resulting string dest
+ * Return: a pointer to the resulting string dest
  */
-char *_strncat(char *dest, const char *src, size_t n)
+char *_strncat(char *dest, char *src, int n)
 {
-	size_t dest_len = 0;
-	char *dest_end;
+	int dest_len, i;
 
-	while (dest[dest_len] != '\0')
-	{
-		dest_len++;
-	}
+	/* get the length of dest */
+	for (dest_len = 0; dest[dest_len]; dest_len++)
+		;
 
-	dest_end = dest + dest_len;
+	/* concatenate up to n bytes from src to dest */
+	for (i = 0; src[i] && i < n; i++)
+		dest[dest_len + i] = src[i];
 
-	while (*src != '\0' && n-- > 0)
-	{
-		*dest_end++ = *src++;
-	}
-
-	*dest_end = '\0';
+	/* add a null terminator to dest */
+	dest[dest_len + i] = '\0';
 
 	return dest;
 }
