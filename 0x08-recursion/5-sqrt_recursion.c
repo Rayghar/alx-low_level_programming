80b7583e70e4ae1a7c6eecf2da20eea7293bd544
Rayghar <chukwuneme@gmail.com   qexitcd ..git statusclosecat README.md ls:q!?>
2023-03-12 14:38:44 -0700

    y

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,7 +5,7 @@
  * _sqrt_recursion - Returns the natural square root of a number.
  * @n: The number to find the natural square root of.
  *
- * Return: The natural square root of n If n does not have a natural square root, return -1.
+ * Return: The nat sq root of n If n doesnt have a nat sq root, return -1.
  */
 int _sqrt_recursion(int n)
 {
@@ -17,12 +17,12 @@ int _sqrt_recursion(int n)
 }
 
 /**
- * _sqrt_recursion_helper - Helps to recursively find the square root of a number.
+ * _sqrt_recursion_helper - Helps to recursively find the sq root of number.
  * @n: The number to find the square root of.
  * @i: The current guess at the square root of n.
  *
  * Return: The square root of n if i * i == n If i * i > n, return -1.
- *         Otherwise, return the result of calling the function recursively with n and i + 1.
+ * else ret the result of calling the fcn recursively with n and i + 1.
  */
 int _sqrt_recursion_helper(int n, int i)
 {
