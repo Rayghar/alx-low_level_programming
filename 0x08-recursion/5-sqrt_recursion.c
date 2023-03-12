97e3e54395fdcedd2a372be66b1e15d39267ad6f
Rayghar <chukwuneme@gmail.com   qexitcd ..git statusclosecat README.md ls:q!?>
2023-03-12 12:04:54 -0700

    y

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,43 +2,24 @@
 #include <stdio.h>
 
 /**
- * _sqrt_recursion - Returns the natural square root of a number
- * @n: The number to find the square root of
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: the number to compute the square root of.
  *
- * Return: The natural square root of n, or -1 if n does not have a natural
- * square root
+ * Return: the natural square root of n, or -1 if n does not have a
+ *         natural square root.
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0) /* Negative numbers don't have a natural square root */
+	if (n < 0) /* negative numbers do not have natural square roots */
 		return (-1);
-
-	if (n == 0 || n == 1) /* The square root of 0 or 1 is itself */
+	if (n == 0 || n == 1) /* the square root of 0 or 1 is itself */
 		return (n);
-	return (_sqrt_helper(n, 1, n / 2));
-}
+	int root = _sqrt_recursion(n / 2);
 
-/**
- * _sqrt_helper - helper function for _sqrt_recursion
- * @n: the number to find the square root of
- * @min: the minimum possible square root
- * @max: the maximum possible square root
- *
- * Return: the square root of n, or -1 if n does not have a natural square root
- */
-int _sqrt_helper(int n, int min, int max)
-{
-	int mid;
-
-	if (max < min) /* square root not found */
+	if (root * root == n)
+		return (root);
+	else if (root * root < n)
+		return _sqrt_recursion((n - root * root) == -1 ? -1 : root);
+	else
 		return -1;
-
-	mid = (min + max) / 2; /* find midpoint */
-
-	if (mid * mid == n) /* square root found */
-		return mid;
-	else if (mid * mid > n) /* search left half */
-		return _sqrt_helper(n, min, mid - 1);
-	else /* search right half */
-		return _sqrt_helper(n, mid + 1, max);
 }
