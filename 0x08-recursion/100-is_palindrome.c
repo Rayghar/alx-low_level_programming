22e02cd968fb7937ae491758c870fac858b8f38b
Rayghar <chukwuneme@gmail.com   qexitcd ..git statusclosecat README.md ls:q!?>
2023-03-12 15:09:16 -0700

    y

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,41 +3,50 @@
 
 /**
  * is_palindrome - checks if a string is a palindrome
- * @s: the string to check
+ * @s: string to check
  *
  * Return: 1 if s is a palindrome, 0 otherwise
  */
 
-int check_palindrome(char *s, int end, int start);
+int _strlen_recursion(char *s);
+int check_palindrome(char *s, int start, int end);
 
 int is_palindrome(char *s)
 {
-	int len = 0;
+	int len;
 
-	if (!*s)
+	len = _strlen_recursion(s);
+	if (len <= 1)
 		return (1);
-	
-	while (s[len])
-		len++;
-
-	return (check_palindrome(s, len - 1, 0));
+	return (check_palindrome(s, 0, len - 1));
 }
 
 /**
- * check_palindrome - recursively checks if a string is a palindrome
- * @s: the string to check
- * @end: the index of the last character in s
- * @start: the index of the first character in s
+ * check_palindrome - checks if a string is a palindrome recursively
+ * @s: string to check
+ * @start: starting index
+ * @end: ending index
  *
  * Return: 1 if s is a palindrome, 0 otherwise
  */
-int check_palindrome(char *s, int end, int start)
+int check_palindrome(char *s, int start, int end)
 {
 	if (start >= end)
 		return (1);
-
 	if (s[start] != s[end])
 		return (0);
+	return (check_palindrome(s, start + 1, end - 1));
+}
 
-	return (check_palindrome(s, end - 1, start + 1));
+/**
+ * _strlen_recursion - returns the length of a string recursively
+ * @s: string to get length of
+ *
+ * Return: length of s
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
