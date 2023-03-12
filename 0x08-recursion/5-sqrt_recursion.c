f08188a903ded6d5b16d63d208505f186af1af34
Rayghar <chukwuneme@gmail.com   qexitcd ..git statusclosecat README.md ls:q!?>
2023-03-12 11:57:31 -0700

    y

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,3 +17,28 @@ int _sqrt_recursion(int n)
 		return (n);
 	return (_sqrt_helper(n, 1, n / 2));
 }
+
+/**
+ * _sqrt_helper - helper function for _sqrt_recursion
+ * @n: the number to find the square root of
+ * @min: the minimum possible square root
+ * @max: the maximum possible square root
+ *
+ * Return: the square root of n, or -1 if n does not have a natural square root
+ */
+int _sqrt_helper(int n, int min, int max)
+{
+	int mid;
+
+	if (max < min) /* square root not found */
+		return -1;
+
+	mid = (min + max) / 2; /* find midpoint */
+
+	if (mid * mid == n) /* square root found */
+		return mid;
+	else if (mid * mid > n) /* search left half */
+		return _sqrt_helper(n, min, mid - 1);
+	else /* search right half */
+		return _sqrt_helper(n, mid + 1, max);
+}
