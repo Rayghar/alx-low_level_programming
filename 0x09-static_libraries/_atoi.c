bc8691ecec023629b2ded6973dae794bf03f4631
Rayghar <chukwuneme@gmail.com   qexitcd ..git statusclosecat README.md ls:q!?>
2023-03-19 06:14:20 -0700

    y

---
+++ 0x09-static_libraries/_atoi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _atoi(char *s) {
+	/* TODO: implement this function */
+	return 0;
+}
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = '_';
+
+	_putchar(c);
+	c = 'p';
+	_putchar(c);
+	c = 'u';
+	_putchar(c);
+	c = 't';
+	_putchar(c);
+	c = 'c';
+	_putchar(c);
+	c = 'h';
+	_putchar(c);
+	c = 'a';
+	_putchar(c);
+	c = 'r';
+	_putchar(c);
+	c = '\n';
+	_putchar(c);
+	return (0);
+}
